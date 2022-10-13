f013d020d3beb0362186ca282eea27dbbb76d7c1
Brian-Acis <114514189+Brian-Acis@users.noreply.github.com>
2022-10-13 09:58:23 -0500

    Add files via upload

---
+++ BrianAcisMonsivaisGonzalez/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>My first php page</h1>
+    <?php
+echo "hello world <br> ";
+ECHO "hello world <br> ";
+eCHo "hello world <br> ";   
+$color =  "red";
+
+echo "my car is" . $color . "<br>";
+echo "my house is" . $COLOR . "<br>"; 
+echo "my boat is" . $coLOR . "<br>";
+$txt = "Hello World"; 
+$x = 5; 
+$y = 10.5; 
+
+echo $txt;
+echo "<br>"; 
+echo $x; 
+echo $y; 
+    ?>
+    
+    
+</body>
+</html>
+
